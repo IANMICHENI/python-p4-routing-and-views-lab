8c973844d4c3fd5158279f675f93f8a9225f6ed3
ian <ianmwendamicheni@gmail.com>
2024-01-08 12:34:37 +0300

    routing

---
+++ app/app.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    return f'<p>String printed: {param}</p>'
+
+@app.route('/count/<int:param>')
+def count(param):
+    numbers = '\n'.join(str(i) for i in range(1, param + 1))
+    return f'<pre>{numbers}</pre>'
+
+@app.route('/math/<int:num1><operation><int:num2>')
+def math(num1, operation, num2):
+    result = None
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return 'Error: Division by zero'
+    elif operation == '%':
+        if num2 != 0:
+            result = num1 % num2
+        else:
+            return 'Error: Modulo by zero'
+    else:
+        return 'Error: Invalid operation'
+
+    return f'<p>Result of {num1} {operation} {num2} is: {result}</p>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
